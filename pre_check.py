c9265fa9661651b7c86c74b5ba73da80c55d8707
Micah_Warren <26724012+MicahWW@users.noreply.github.com>
2025-10-30 23:30:37 -0500

    publish a release

--- pre_check.py
+++ pre_check.py
@@ -8,14 +8,13 @@ module writes an error message to stdout in the GitHub Actions error format and
 exits the process with a non-zero status.
 
 Behavior and checks performed:
-- Reads expected inputs from environment variables:
-    - INPUT_TIMEZONE: timezone name to be validated via pytz
-    - INPUT_PUSH_TAG: string flag expected to be "true" or "false"
-    - INPUT_GITHUB_TOKEN: token required when pushing tags
 - Validates that INPUT_TIMEZONE corresponds to a known pytz timezone.
 - Validates that INPUT_PUSH_TAG is either "true" or "false".
+- Validates that INPUT_PUBLISH_RELEASE is either "true" or "false".
 - If pushing tags is requested (INPUT_PUSH_TAG == 'true'), verifies that
   INPUT_GITHUB_TOKEN is present and non-empty.
+- If publishing a release is request (INPUT_PUBLISH_RELEASE == 'true'), verifies
+  that INPUT_PUSH_TAG is set to true.
 
 Exit behavior:
 - On any validation failure, the module prints an error line prefixed with
@@ -39,6 +38,7 @@ import pytz
 timezone = os.getenv('INPUT_TIMEZONE')
 push_tag = os.getenv('INPUT_PUSH_TAG')
 github_token = os.getenv('INPUT_GITHUB_TOKEN')
+publish_release = os.getenv('INPUT_PUBLISH_RELEASE')
 
 # region Check values are set
 if (not timezone):
@@ -48,6 +48,11 @@ if (not timezone):
 if (not push_tag):
     print('::error::Default push_tag not set via actions.yml')
     exit(1)
+
+if (not publish_release):
+    print('::error::Default publish_release not set via actions.yml')
+    exit(1)
+
 # endregion
 
 # region Check timezone
@@ -58,11 +63,18 @@ except pytz.UnknownTimeZoneError:
     exit(1)
 # endregion
 
-# region Check github_token
+# region Check input allowed values
 if (push_tag not in ['true', 'false']):
     print(f'::error::Unexpected input for push_tag of "{push_tag}"')
     exit(1)
 
+if (publish_release not in ['true', 'false']):
+    print(f'::error::Unexpected input for publish_release of "{publish_release}"')
+    exit(1)
+
+# endregion
+
+# region Check github token
 if (not push_tag and push_tag == 'true'):
     if (not github_token or not github_token.strip()):
         print('::error::github_token is required ')
